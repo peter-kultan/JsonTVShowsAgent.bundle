dff37604bb54f4c2eb393173dd545f2e6e7b41ce
Peter Kultán <xkultan@mail.muni.cz>
2022-12-02 12:55:12 +0100

    feat: finish implementation of update function

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,8 +1,10 @@
 import os, json, re
+from dateutil.parser import parse
 
 load_file = Core.storage.load
+MediaProxy = Proxy.Media
 
-class JsonTVShowsAfent(Agent.TV_Shows):
+class JsonTVShowsAgent(Agent.TV_Shows):
 
     name = 'JsonTVShowsAgent'
     primary_provider = True
@@ -31,34 +33,120 @@ class JsonTVShowsAfent(Agent.TV_Shows):
         results.Append(MetadataSearchResult(id=media.id, name=media.show, lang=lang, score=100))
 
     def update(self, metadata, media, lang):
-        for season in media.children:
-            self.updateSeason(metadata, season, lang)
-    
-    def updateSeason(self, metadata, media, lang):
-        for episode in media.children:
-            self.updateEpisode(metadata, episode, lang)
+        dictionary, directory = get_episodes_from_show(media)
 
-    def updateEpisode(self, metadata, media, lang):
-        log.debug(media.items[0].parts[0].file)
+        path = os.path.join(directory, 'Info.json')
 
-        title, season, episode, directory = getShowSeasonAndEpisodeFromPath(media.items[0].parts[0].file)
+        if not os.path.exists(path):
+            return
 
-        log.debug(title)
-        log.debug(season)
-        log.debug(episode)
-        log.debug(directory)
-        log.debug(os.path.exists(os.path.join(directory, "Info.json")))
+        info = json.loads(load_file(path))
+
+        for show, seasons in dictionary.items():
+            show_info = info[show]
+
+            try: metadata.title = show_info['title']
+            except: pass
+
+            try: metadata.original_title = show_info['original_title']
+            except: pass
+
+            try: metadata.originally_available_at = parse(show_info['originally_available_at'])
+            except: pass
+
+            try: metadata.summary = show_info['summary']
+            except: pass
+
+            try: metadata.rating = show_info['rating']
+            except: pass
+
+            try: metadata.studio = show_info['studio']
+            except: pass
+
+            try: 
+                poster_path = show_info['poster_path']
+                poster = load_file(poster_path)
+                metadata.posters[poster_path] = MediaProxy(poster)
+            except: 
+                pass
+
+            metadata.genres.clear()
+
+            try:
+                for g in show_info['genres']:
+                    metadata.genres.add(g)
+            except:
+                pass
+
+            metadata.countries.clear()
+
+            try:
+                for d in show_info['countries']:
+                    metadata.countries.add(d)
+            except:
+                pass
+
+            self.update_seasons(metadata, seasons, show_info)
+
+
+    def update_seasons(self, metadata, dictionary, info):
+        for season, episodes in dictionary.items():
+            try: 
+                season_info = info['seasons'][str(season).zfill(2)]
+                season_metadata = metadata.seasons[season]
+            except: continue
+
+            try: season_metadata.title = season_info['title']
+            except: pass
+
+            try: season_metadata.summary = season_info['summary']
+            except: pass
+
+            self.update_episodes(season_metadata, episodes, season_info)
+
+
+    def update_episodes(self, metadata, episodes, info):
+        for episode in episodes:
+            try: 
+                episode_info = info['episodes'][str(episode).zfill(2)]
+                episode_metadata = metadata.episodes[episode]
+            except:
+                continue
+
+            try: episode_metadata.title = episode_info['title']
+            except: pass
+
+            try: episode_metadata.originally_available_at = parse(episode_info['originally_available_at'])
+            except: pass
+
+            try: episode_metadata.summary = episode_info['summary']
+            except: pass
+
+            try: episode_metadata.rating = episode_info['rating']
+            except: pass
 
 
 # Help function
 
-def getShowSeasonAndEpisodeFromPath(path):
+
+def get_episodes_from_show(media):
+    dictionary = dict()
+
+    for ch in media.children:
+        if len(ch.items) > 0:
+            directory = get_show_seasons_episodes_from_path(ch.items[0].parts[0].file, dictionary)
+        else:
+            dictionary, directory = get_episodes_from_show(ch)
+
+    return dictionary, directory
+
+def get_show_seasons_episodes_from_path(path, dictionary):
     file = os.path.basename(path)
     title = season = episode = directory = ""
 
 
     if re.search("^(e|E){0,1}[0-9]{1,}\.[^.]*$", file):
-        episode = (file[1:] if re.search("^(e|E)", file) else file).split('.')[0]
+        episode = int((file[1:] if re.search("^(e|E)", file) else file).split('.')[0])
 
         dirpath = os.path.dirname(path)
 
@@ -83,7 +171,15 @@ def getShowSeasonAndEpisodeFromPath(path):
         episode = int(res.group(5))
         directory = os.path.dirname(path)
 
-    return title, season, episode, directory
+    if dictionary.get(title) is None:
+        dictionary[title] = dict()
+
+    if dictionary[title].get(season) is None:
+        dictionary[title][season] = []
+
+    dictionary[title][season].append(episode)
+
+    return directory
 
 
 class PlexLogAdapter(object):
